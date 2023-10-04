8282f87fe64daa6daab4216911932c415364e52c
Dimasadhy <113024825+Dimasadhy@users.noreply.github.com>
2023-10-04 14:50:53 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+function Login(username,pass){
+  console.log('---------------')
+  console.log(' login ')
+  console.log('---------------')
+  console.log('username :'+(username))
+  console.log('pass :'+(pass))
+  console.log('---------------')
+  
+  
+}
+Login('dimas','######')
